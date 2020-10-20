309570f6a4fd63f87f1d69496569388d53db14ce
Reza-CyBer44 <64094365+Reza-CyBer44@users.noreply.github.com>
2020-10-20 23:55:20 +0330

    Create SQL.sql

---
+++ SQL.sql
@@ -0,0 +1,28 @@
+
+INSERT INTO `jobs` (`name`, `label`) VALUES
+('russia', 'Russia'),
+('unemployed', 'Unemployed'),
+('usa', 'United State America');
+
+
+INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+(1, 'unemployed', 0, 'unemployed', 'Unemployed', 200, '{}', '{}'),
+(2, 'russia', 0, 'unselected', 'Select Your Division', 0, '{}', '{}'),
+(3, 'russia', 1, 'comrifle', 'Rifle Commando', 0, '{}', '{}'),
+(4, 'russia', 2, 'comartil', 'Artillery Commando', 0, '{}', '{}'),
+(5, 'russia', 3, 'comshotgun', 'Shotgun Commando', 0, '{}', '{}'),
+(6, 'russia', 4, 'comsnip', 'Sniper Commando', 0, '{}', '{}'),
+(7, 'russia', 5, 'gpilot', 'Ground Pilot', 0, '{}', '{}'),
+(8, 'russia', 6, 'apilot', 'Aircraft Pilot', 0, '{}', '{}'),
+(9, 'usa', 0, 'unselected', 'Select Your Division', 0, '{}', '{}'),
+(10, 'usa', 1, 'comrifle', 'Rifle Commando', 0, '{}', '{}'),
+(11, 'usa', 2, 'comartil', 'Artillery Commando', 0, '{}', '{}'),
+(12, 'usa', 3, 'comshotgun', 'Shotgun Commando', 0, '{}', '{}'),
+(13, 'usa', 4, 'comsnip', 'Sniper Commando', 0, '{}', '{}'),
+(14, 'usa', 5, 'gpilot', 'Ground Pilot', 0, '{}', '{}'),
+(15, 'usa', 6, 'apilot', 'Aircraft Pilot', 0, '{}', '{}'),
+(16, 'russia', 7, 'gcommander', 'Ground Squad Commander', 0, '{}', '{}'),
+(17, 'russia', 8, 'acommander', 'Aircraft Squad Commander', 0, '{}', '{}'),
+(18, 'usa', 7, 'gcommander', 'Ground Squad Commander', 0, '{}', '{}'),
+(19, 'usa', 8, 'acommander', 'Aircraft Squad Commander', 0, '{}', '{}');
+
